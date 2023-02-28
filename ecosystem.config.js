feec3cd6f47054611a90b8af891a7da67f219196
Aka'aba Musa Akidi <onlinemraam@gmail.com>
2023-02-28 09:49:17 +0100

    beta deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "PhoneKit Admin Dashboard",
-      port: "5405",
+      port: 5405,
       exec_mode: "cluster",
       instances: "max",
       script: "./.output/server/index.mjs",
